f346f92bf1c9edf7e75fd322eefe0a4e67abdfdc
Doug Winter <doug@isotoma.com>
2010-03-21 18:25:39 +0000

    no longer required it seems

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-virtual=`realpath virtual`
-thirdparty=`realpath thirdparty`
-export PATH=$virtual/bin:$PATH
-export PKG_CONFIG_PATH="$thirdparty/libspotify/lib/pkgconfig"
-export LD_LIBRARY_PATH="$thirdparty/libspotify/lib"
-export PYTHONPATH=`realpath .`
